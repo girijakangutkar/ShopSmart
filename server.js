f63156adf83549a6c5218531e47e530e9f3f2897
Girija-Kangutkar <kangutkargirija075@gmail.com>
2025-08-10 00:19:21 +0530

    Payment razor passed for sometime

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const AuthRouter = require("./routes/AuthRouter");
 const Logger = require("./middlewares/Logger");
 const UserRouter = require("./routes/UserRouter");
 const ProductRouter = require("./routes/ProductRouter");
-const Razorpay = require("razorpay");
+// const Razorpay = require("razorpay");
 const app = express();
 
 console.log(process.env.NODE_ENV);
@@ -30,10 +30,10 @@ app.use(
   })
 );
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
+// const razorpay = new Razorpay({
+//   key_id: process.env.RAZORPAY_KEY_ID,
+//   key_secret: process.env.RAZORPAY_KEY_SECRET,
+// });
 
 app.use(express.json());
 
@@ -43,20 +43,20 @@ app.use("/myInfo", Logger, UserRouter);
 
 app.use("/wareHouse", Logger, ProductRouter);
 
-app.post("/OrderPayment", async (req, res) => {
-  const { amount } = req.body;
+// app.post("/OrderPayment", async (req, res) => {
+//   const { amount } = req.body;
 
-  try {
-    const order = await razorpay.orders.create({
-      amount: amount * 100,
-      currency: "INR",
-      receipt: "receipt_" + Date.now(),
-    });
-    res.json(order);
-  } catch (error) {
-    res.status(500).json({ msg: "Something went wrong", error });
-  }
-});
+//   try {
+//     const order = await razorpay.orders.create({
+//       amount: amount * 100,
+//       currency: "INR",
+//       receipt: "receipt_" + Date.now(),
+//     });
+//     res.json(order);
+//   } catch (error) {
+//     res.status(500).json({ msg: "Something went wrong", error });
+//   }
+// });
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(3000, () => {
